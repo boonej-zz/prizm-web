64eeec3d31b478251fa6b9d7340b902db495ed7c
Jonathan Boone <jb@moonshineinteractive.com>
2014-06-03 17:20:04 -0500

    added db

---
+++ db/db.js
@@ -0,0 +1,15 @@
+var mongoURI = process.env.MONGOHQ_URL || 'mongodb://localhost/prizm';
+
+var mongoose = require('mongoose');
+
+var recordSchema = new mongoose.Schema({
+  name:       String,
+  age:        Number,
+  gender:     String,
+  cityState:  String,
+  email:      String,
+  mobile:     String
+});
+
+mongoose.model('Record', recordSchema);
+mongoose.connect(mongoURI);
