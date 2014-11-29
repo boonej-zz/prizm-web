8254f387b25431e8a562b82f2de83d0452ed67c6
Jonathan Boone <jb@higheraltitude.com>
2014-11-28 22:51:20 -0600

    removing unneeded pointer

--- app.js
+++ app.js
@@ -61,7 +61,6 @@ app.use(function(req, res, next){
 app.use(subdomain('admin', admin));
 
 app.use(subdomain('*', routes));
-app.use(subdomain('www', routes));
 
 
 /// catch 404 and forwarding to error handler
