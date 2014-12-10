f49698d331deb9ec6dca6852024355e4f6494dec
Eric Kenny <ekenny82@gmail.com>
2014-12-10 11:21:18 -0600

    smooth scrolling animation for navbar

--- public/javascripts/prizm.js
+++ public/javascripts/prizm.js
@@ -86,6 +86,21 @@ $(window).scroll(function(){
     }
   }
 })
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 600);
+        return false;
+      }
+    }
+  });
+});
 /*
 $(function() {
   var input_element = document.getElementById("interest-selection");
