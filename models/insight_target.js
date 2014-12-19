a702e45fdcc8c0dc9c2108761865f0d8700de31f
Eric Kenny <ekenny82@gmail.com>
2014-12-18 23:02:19 -0600

    model cleanup

--- models/insight_target.js
+++ models/insight_target.js
@@ -1,10 +1,11 @@
 var mongoose = require('mongoose');
+var ObjectId = mongoose.Schema.Types.ObjectId;
 
 var insightTargetSchema = new mongoose.Schema({
   create_date     : {type: Date, default: null, required: false, index: true},
-  insight         : {type: mongoose.Schema.Types.ObjectId, ref: 'Insight', required: true},
-  creator         : {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
-  target          : {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+  insight         : {type: ObjectId, ref: 'Insight', required: true},
+  creator         : {type: ObjectId, ref: 'User', required: true},
+  target          : {type: ObjectId, ref: 'User', required: true},
   liked           : {type: Boolean, default: false},
   disliked        : {type: Boolean, default: false},
   file_path       : {type: String, default: null}
