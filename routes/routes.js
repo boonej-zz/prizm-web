02bf22790b718551e131d5d222c8a976e0330a0f
Eric Kenny <ekenny82@gmail.com>
2014-12-31 12:46:06 -0600

    Clean up routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,68 +1,12 @@
-var express = require('express');
-var router = express.Router();
-var mongoose = require('mongoose');
-var _posts = require('../controllers/posts');
-var _users = require('../controllers/users');
+var express   = require('express');
+var router    = express.Router();
+var mongoose  = require('mongoose');
+var utils     = require('../utils');
+var _         = require('underscore');
+var _posts    = require('../controllers/posts');
+var _users    = require('../controllers/users');
 var _organizations = require('../controllers/organizations');
-var Activity = mongoose.model('Activity');
-var Record = mongoose.model('Record');
-var Post = mongoose.model('Post');
-var User = mongoose.model('User');
-var Insight = mongoose.model('Insight');
-var InsightTarget = mongoose.model('InsightTarget');
-var Interest = mongoose.model('Interest');
-var Organization = mongoose.model('Organization');
-var Push = require('../classes/push_notification');
-var config = require('../config');
-var mandrill = require('node-mandrill')(config.mandrill.client_secret);
-var mandrillEndpointSend = '/messages/send';
-var ejs = require('ejs');
-var jade = require('jade');
-var fs = require('fs');
-var path = require('path');
-var mail = fs.readFileSync(path.join(__dirname + '/../lib/mail.ejs'), 'utf8');
-var rejectMail = fs.readFileSync(path.join(__dirname +
-      '/../views/reject_mail.jade'), 'utf8');
-var acceptMail = fs.readFileSync(path.join(__dirname +
-      '/../views/accept_mail.jade'), 'utf8');
-var adminBody = fs.readFileSync(path.join(__dirname + 
-      '/../views/adminMail.ejs'), 'utf8');
-var postFeed = fs.readFileSync(path.join(__dirname +
-      '/../views/post_feed.jade'), 'utf8');
-var adminEmail = 'info@prizmapp.com';
-var ObjectId = require('mongoose').Types.ObjectId;
-var moment = require('moment');
-var utils = require('../utils');
-var validateEmail = require('../utils').validateEmail;
-var uuid = require('../utils').generateUUID;
-var aws = require('aws-sdk');
-var _ = require('underscore');
 
-var AWS_ACCESS_KEY = 'AKIAJ656TNM2SYQUMHCA';
-var AWS_SECRET_KEY = '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'; 
-var S3_BUCKET = 'higheraltitude.prizm.insights'; 
-
-moment.relativeTimeThreshold('d', 6);
-moment.relativeTimeThreshold('M', 52);
-
-moment.locale('en', {
-  relativeTime: {
-    past: "%s",
-    s:  "%ds",
-    m:  "%dm",
-    mm: "%dm",
-    h:  "%dh",
-    hh: "%dh",
-    d:  "%dd",
-    dd: "%dd",
-    M:  "4w",
-    MM: function(number, withoutSuffix, key, isFuture){
-      return number*4 + 'w';
-    },
-    y:  "%dy",
-    yy: "%dy"
-                } 
-});
 
 /* Website */
 router.get('/', function(req, res) {
