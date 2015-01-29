dfcbb34adb1e15910c2e3e97e4bbcfeb14106eed
Eric Kenny <ekenny82@gmail.com>
2015-01-29 15:25:16 -0600

    Add ajax requests to get profile members by status

---
+++ public/javascripts/profile/dashboard.js
@@ -0,0 +1,41 @@
+/* Members Page */
+$(function(){
+  $('li a[href="#pending"]').click(function(){
+    var request = $.ajax({
+      type: 'GET',
+      url: window.location.pathname,
+      headers: {
+        'Accept': 'application/jade',
+        'memberStatus': 'pending'
+      },
+      success: function(html) {
+        if (html) {
+          $('#pending-members').html(html);
+        }
+      }
+    });
+    request.done(function(){
+      $('#active-members').hide()
+      $('#pending-members').fadeIn();
+    });
+  });
+  $('li a[href="#active"]').click(function(){
+    var request = $.ajax({
+      type: 'GET',
+      url: window.location.pathname,
+      headers: {
+        'Accept': 'application/jade',
+        'memberStatus': 'active'
+      },
+      success: function(html) {
+        if (html) {
+          $('#active-members').html(html);
+        }
+      }
+    });
+    request.done(function(){
+      $('#pending-members').hide()
+      $('#active-members').fadeIn();
+    });
+  });
+});
