3b7c5ebe788a1d2a188e9d7c70439ea9ec7961c6
Eric Kenny <ekenny82@gmail.com>
2015-01-29 19:33:38 -0600

    Left protected routes unprotected

--- routes/routes.js
+++ routes/routes.js
@@ -57,7 +57,7 @@ router.get('/posts/:id', _posts.singlePost)
 router.get('/users/:id/password', _users.passwordReset);
 router.get('/users', utils.auth, _users.fetchUsers);
 router.get('/users/:id/institutions', _users.institutionApproval);
-router.post('/users/:id', _users.updateOrgStatus);
+router.post('/users/:id', _users.authRequired, _users.updateOrgStatus);
 
 /* Authorization */
 router.get('/login', _users.displayLogin);
@@ -69,7 +69,7 @@ router.get('/logout', _users.handleLogout);
 /* Profiles */
 router.get('/profile', _users.authRequired, _users.displayProfile);
 router.get('/profile/:id', _users.displayProfileById)
-router.get('/profile/:id/members', /*_users.authRequired,*/ _users.displayMembers);
+router.get('/profile/:id/members', _users.authRequired, _users.displayMembers);
 
 /** Organization Pages **/
 router.get('/:name', _organizations.displayOrganization);
