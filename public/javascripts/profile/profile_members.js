0fc5b9f0ac76992b43d40c143325da6aad5cce81
Eric Kenny <ekenny82@gmail.com>
2015-01-31 17:00:58 -0600

    Finish profile member view layout

--- public/javascripts/profile/profile_members.js
+++ public/javascripts/profile/profile_members.js
@@ -1,26 +1,59 @@
 /* Members Page */
-$(function(){
-  // Handle Members Active/Pending Tab Toggle
-  $('li a[href="#pending"]').click(function(){
-    var request = $.ajax({
-      type: 'GET',
-      url: window.location.pathname,
-      headers: {
-        'Accept': 'application/jade',
-        'memberStatus': 'pending'
+
+var members = {
+  // Member Action Methods
+  approve: function(member_id) {
+    var organization = $('#organization').attr('data');
+    $.ajax({
+      type: 'POST',
+      url: '/users/' + member_id,
+      headers:{
+        'Accept': 'application/json',
+        'org': organization,
+        'status': 'active'
       },
-      success: function(html) {
-        if (html) {
-          $('#pending-members').html(html);
-        }
+      success: function() {
+        members.pendingTab();
+        members.updateActiveCount();
+        members.updatePendingCount();
       }
     });
-    request.done(function(){
-      $('#active-members').hide()
-      $('#pending-members').fadeIn();
+  },
+  reject: function(member_id) {
+    var organization = $('#organization').attr('data');
+    $.ajax({
+      type: 'POST',
+      url: '/users/' + member_id,
+      headers:{
+        'Accept': 'application/json',
+        'org': organization,
+        'status': 'inactive'
+      },
+      success: function() {
+        members.pendingTab();
+        members.updatePendingCount();
+      }
     });
-  });
-  $('li a[href="#active"]').click(function(){
+  },
+  remove: function(member_id) {
+    var organization = $('#organization').attr('data');
+    $.ajax({
+      type: 'POST',
+      url: '/users/' + member_id,
+      headers:{
+        'Accept': 'application/json',
+        'org': organization,
+        'status': 'inactive'
+      },
+      success: function() {
+        members.activeTab();
+        members.updateActiveCount();
+      }
+    });
+  },
+
+  // Member Page Controls
+  activeTab: function() {
     var request = $.ajax({
       type: 'GET',
       url: window.location.pathname,
@@ -35,8 +68,68 @@ $(function(){
       }
     });
     request.done(function(){
-      $('#pending-members').hide()
+      $('#pending-members').hide();
       $('#active-members').fadeIn();
+    });   
+  },
+
+  pendingTab: function() {
+    var request = $.ajax({
+      type: 'GET',
+      url: window.location.pathname,
+      headers: {
+        'Accept': 'application/jade',
+        'memberStatus': 'pending'
+      },
+      success: function(html) {
+        if (html) {
+          $('#pending-members').html(html);
+        }
+      }
     });
+    request.done(function(){
+      $('#active-members').hide();
+      $('#pending-members').fadeIn();
+    });
+  },
+
+  updateActiveCount: function(){
+    $.ajax({
+      type: 'GET',
+      url: window.location.pathname,
+      headers: {
+        'Accept': 'application/json',
+        'memberStatus': 'active'
+      },
+      success: function(json) {
+        $('a[href="#active"]').text("Approved Members (" + json.length + ")");
+      }
+    })
+  },
+
+  updatePendingCount: function(){
+    $.ajax({
+      type: 'GET',
+      url: window.location.pathname,
+      headers: {
+        'Accept': 'application/json',
+        'memberStatus': 'pending'
+      },
+      success: function(json) {
+        console.log("Pending count: " + json.length);
+        $('a[href="#pending"]').text("Pending Members (" + json.length + ")");
+      }
+    })
+  }
+};
+
+
+$(function(){
+  // Get Active and Pending Tab Counts
+  members.updateActiveCount();
+  members.updatePendingCount();
+
+  $('.restrict').on('click', function(){
+    $(this).find('.restrict-menu').toggleClass('hidden');
   });
 });
