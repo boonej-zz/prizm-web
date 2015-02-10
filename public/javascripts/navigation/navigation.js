4f424d10ebd89749e6726df6f8b9ff6d28ed1590
Eric Kenny <ekenny82@gmail.com>
2015-02-10 10:16:51 -0800

    Finish new navbar

---
+++ public/javascripts/navigation/navigation.js
@@ -0,0 +1,13 @@
+var nav = {
+  toggleMenu: function() {
+    var windowWidth = $(window).width();
+    var menu = $('.user-menu');
+    $('.tool-tip').toggleClass('rotated');
+    if (windowWidth > 500) {
+      menu.toggle();
+    }
+    else {
+      menu.slideToggle();
+    }
+  }
+} 
