b820e2b2b5b6a5c64ce70e03680e1e14855bfc4c
Eric Kenny <ekenny82@gmail.com>
2015-02-18 15:54:21 -0600

    finish user creation for both individual and partner

--- public/javascripts/registration/registration.js
+++ public/javascripts/registration/registration.js
@@ -9,17 +9,51 @@ var reg = {
   partnerForm: function() {
     $('#individual-form').hide();
     $('#partner-form').fadeIn();
-  },
+  }
 
-  registerUser: function(){
-    var user = $('form').serialize();
+  // registerUser: function(event){
+  //   event.preventDefault();
+  //   console.log("YES");
+  //   var user = $('form').serialize();
+  //   $.ajax({
+  //     type: 'POST',
+  //     url: window.location,
+  //     data: user,
+  //     success: function(response) {
+  //       alert('Success');
+  //       // console.log(response);
+  //     },
+  //     error: function(response) {
+  //       alert('Error');
+  //       $('.message').html(response.error);
+  //     }
+  //   });
+  // }
+}
+
+$(function(){
+  $('.form-register').submit(function(){
+    thisForm = "#" + $(this).parent().attr('id') + " form"
+    var user = $(thisForm).serialize();
     $.ajax({
       type: 'POST',
       url: window.location,
       data: user,
       success: function(response) {
-        console.log(response);
+        $('section').addClass('section-1');
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        $('.message').html(jqXHR.responseText);
       }
     });
-  }
-}
+    return false;
+  });
+});
+
+$(function(){
+  $('body').on('click', function(){
+    console.log("CLICK");
+    $('.section').toggleClass('section-1');
+  })
+})
+
