8328003b39a6ea32af1387d8331679d9a59aca42
Jonathan Boone <jb@higheraltitude.com>
2015-02-27 09:29:32 -0600

    rever

--- public/javascripts/profile/profile.js
+++ public/javascripts/profile/profile.js
@@ -28,16 +28,11 @@ function animatePosts() {
     }
   }
 }
-
-var listening = true;
-
 $(document).ready(function(){
   animatePosts();
   var listening = true;
 $(window).scroll(function() {
-  if (listening){
   if($(window).scrollTop() >= $(document).height() - $(window).height()) {
-    listening = false;
     var lastPost = $('.post').last().attr('id');
     var creator = $('.profile-owner').attr('id');
     var feedType;
@@ -63,12 +58,10 @@ $(window).scroll(function() {
     .done(function(html){
       $(".infinite-feed").append(html);
       $('img.lazy').lazyload();
-      listening = true;
     });
   }
   animatePosts();
   });
-  }
 });
 
 
