cce2d28579f70edc86b61cb823622b26922a564a
Jonathan Boone <jb@higheraltitude.com>
2015-03-12 13:20:39 -0500

    style tweaks

--- public/javascripts/profile/profile.js
+++ public/javascripts/profile/profile.js
@@ -286,6 +286,8 @@ var profile = {
     var key = e.which;
     if (key == 13) {
       e.preventDefault();
+      $(e.target).blur();
+      $(e.target).siblings('a').click();
       return false;
     } else {
       return true;
