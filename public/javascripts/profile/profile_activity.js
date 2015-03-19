114bf123531a08386b6ba700b53b80bb79fe2efd
Eric Kenny <ekenny82@gmail.com>
2015-03-19 10:32:44 -0500

    add accept/deny trust requests to activity

--- public/javascripts/profile/profile_activity.js
+++ public/javascripts/profile/profile_activity.js
@@ -6,5 +6,35 @@ var activity = {
   requestTab: function() {
     $('#notifications').hide();
     $('#requests').fadeIn();
+  },
+  acceptRequest: function(e) {
+    var trustId = $(e.target).parent('td').data('trustId');
+    $.ajax({
+      type: 'POST',
+      url: '/trusts/' + trustId,
+      headers: {
+        'Accept': 'application/json',
+        'Content-type': 'application/json',
+        'status': 'accepted'
+      },
+      success: function() {
+
+      }
+    });
+  },
+  denyRequest: function(e) {
+    var trustId = $(e.target).parent('td').data('trustId');
+    $.ajax({
+      type: 'POST',
+      url: '/trusts/' + trustId,
+      headers: {
+        'Accept': 'application/json',
+        'Content-type': 'application/json',
+        'status': 'cancelled'
+      },
+      success: function() {
+
+      }
+    });
   }
 }
