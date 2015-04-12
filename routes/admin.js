2fdea4f600064ae802032aab567300e98f4eced5
Jonathan Boone <jb@higheraltitude.com>
2015-04-11 21:36:01 -0500

    program code fix

--- routes/admin.js
+++ routes/admin.js
@@ -325,7 +325,7 @@ var processSingleUserByEmail = function(email, next) {
 };
 
 var processUsersByProgramCode = function(programCode, next){
-  user.find({program_code: programCode}, function(err, users){
+  User.find({program_code: programCode}, function(err, users){
     next(err, users);
   });
 };
