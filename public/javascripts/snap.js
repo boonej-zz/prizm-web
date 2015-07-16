60094c1d6bd1aa39da978f7d3e8267e45e4c2c27
Jonathan Boone <jb@higheraltitude.com>
2015-07-16 15:31:49 -0500

    disabled scroll lock for mobile

--- public/javascripts/snap.js
+++ public/javascripts/snap.js
@@ -7,36 +7,38 @@ var tid;
 var d;
 
 function scrollHandler(){
-  clearTimeout(tid); 
-  if (!isScrolling) {
-    $('.dotNav').hide();
+  if ($(window).width() > 500) {
+    clearTimeout(tid); 
+    if (!isScrolling) {
+      $('.dotNav').hide();
+    }
+    isScrolling = true;
+    tid = setTimeout(function(){
+      var ds = $('body').scrollTop();
+      var pt = 0;
+      $('.snap').each(function(i){
+        var $this = $(this);
+        var etop = $this.offset().top;
+        if (Math.abs(ds - etop) < $this.height()/2) {
+          $('body').animate({
+            scrollTop: etop - 75
+          }, 300, function(){
+            if (isScrolling && etop != 75) {
+              if (i%2 != 0) {
+                $('.dotNav').addClass('blue-nav');
+              } else {
+                $('.dotNav').removeClass('blue-nav');
+              }
+              $('.dotNav').fadeIn(function(){
+                isScrolling = false;
+              });
+            } 
+          });
+          return false;
+        } 
+      });
+    }, 50);
   }
-  isScrolling = true;
-  tid = setTimeout(function(){
-    var ds = $('body').scrollTop();
-    var pt = 0;
-    $('.snap').each(function(i){
-      var $this = $(this);
-      var etop = $this.offset().top;
-      if (Math.abs(ds - etop) < $this.height()/2) {
-        $('body').animate({
-          scrollTop: etop - 75
-        }, 300, function(){
-          if (isScrolling && etop != 75) {
-            if (i%2 != 0) {
-              $('.dotNav').addClass('blue-nav');
-            } else {
-              $('.dotNav').removeClass('blue-nav');
-            }
-            $('.dotNav').fadeIn(function(){
-              isScrolling = false;
-            });
-          } 
-        });
-        return false;
-      } 
-    });
-  }, 50);
 }
 
 $(document).ready(function(){
